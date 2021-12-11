ae0f56b373f9089846d49e9150492205ff9ca030
Johnatan Chaves Ribeiro <johnatan.jcr@gmail.com>
2021-12-10 21:25:22 -0300

    conectando o portal a uma carteira metamask

--- src/App.js
+++ src/App.js
@@ -1,10 +1,63 @@
+import React, { useEffect, useState  } from "react";
 import './App.css';
 
 function App() {
-
+  const [currentAccount, setCurrentAccount] = useState("");
   const wave = () => {
-
   }
+  
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+      
+      /*
+      * Check if we're authorized to access the user's wallet
+      */
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+      
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account)
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]); 
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  /*
+    * This runs our function when the page loads.
+    */
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
 
   return (
     <div className="mainContainer">
@@ -16,13 +69,23 @@ function App() {
 
         <div className="bio">
           <p>Olá, sou o Baguete.</p>
-          <p>Manda uma mensagem legal!</p>
-          <p>💰 Você poderá ganhar algum ETH 💲</p>
+          <p>Conecte sua carteira Ethereum e me manda uma mensagem legal!</p>
+          <p>💰 Você poderá ganhar algum ETH. 💲</p>
         </div>
 
         <button className="waveButton" onClick={wave}>
           Me conta ai
         </button>
+
+        {/*
+        * If there is no currentAccount render this button
+        */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+        
       </div>
     </div>
   );
